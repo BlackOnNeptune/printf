6c303b910014c7b7f03ce7d77e407e1d5b4f041d
Kroser-ayoub <draidayoub3@gmail.com>
2023-07-25 17:47:01 +0100

    Revert "printf project"
    This reverts commit 057fc8801e48d869d291befe84f275b7934c9c42.

--- _printf.c
+++ _printf.c
@@ -1,45 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- *_printf - print function
- *@format: list of argument types
- *Return: 0 for success
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	int i, j;
-	int format_len, counter = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_list args;
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
-	format_len = my_strlen(format);
-	for (i = 0; i < format_len; i++)
-	{
-		if (format[i + 1] == ' ')
-		{
-			for (j = 0; *(format + j + i + 1) == ' '; j++)
-			{
+	va_start(list, format);
 
-			}
-			if (format[j + i + 1] == '\0')
-				return (-1);
-		}
-		if (*(format + i) == '%')
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
 		{
-			counter = my_switch(args, format[i + 1], counter);
-			i++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			my_putchar(*(format + i));
-			counter++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
-	va_end(args);
-	return (counter);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
